ffc89f1669c738f6b204c12c39503627f7c8d30e
namancg <naman.c@perfios.com>
2021-09-21 19:39:44 +0530

    REFACTORED: test case to check wrong csv file path

--- src/test/java/com/bridgelabz/statecensusanalyser/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/statecensusanalyser/CensusAnalyserTest.java
@@ -1,9 +1,11 @@
 package com.bridgelabz.statecensusanalyser;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CensusAnalyserTest {
 	private static final String INDIA_STATE_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndianStateCensusData.csv";
+	private static final String WRONG_CSV_FILE_PATH = "./src/main/com/bridgelabz/StateCensusAnaylyser/IndianStateCensusData.csv";
 	
 	@Test
 	public void givenIndianCensusCSVFile_WhenCorrectPath_ShouldReturnCorrectRecords() 
@@ -14,6 +16,19 @@ public class CensusAnalyserTest {
 			Assert.assertEquals(33, numOfRecords);
 		} 
 		catch (CensusAnalyserException e) {
+		}
+	}
+	@Test
+	public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+		try {
+			StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CensusAnalyserException.class);
+			censusAnalyser.LoadCensusData(WRONG_CSV_FILE_PATH);
+		} 
+		catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+			e.printStackTrace();
 		}
 	}
 	
