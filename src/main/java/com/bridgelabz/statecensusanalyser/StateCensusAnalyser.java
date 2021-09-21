4807adec44bdc47104d9efd362b05d71cfad6f81
namancg <naman.c@perfios.com>
2021-09-21 20:41:33 +0530

    REFACTORED: method to load state code csv file

--- src/main/java/com/bridgelabz/statecensusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/StateCensusAnalyser.java
@@ -7,11 +7,16 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.stream.StreamSupport;
 
+import com.bridgelabz.statecensusanalyser.CensusAnalyserException.ExceptionType;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class StateCensusAnalyser {
 	public int LoadCensusData(String csvFilePath) throws CensusAnalyserException {
+		if(csvFilePath.contains("txt"))
+		{
+			throw new CensusAnalyserException("",ExceptionType.CENSUS_FILE_PROBLEM);
+		}
 		try {
 	        Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
 	        CsvToBeanBuilder<CSVStateCensus> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
@@ -33,4 +38,25 @@ public class StateCensusAnalyser {
 	                                         CensusAnalyserException.ExceptionType.INVALID_HEADER_AND_DELIMITER);
 	    }
 	}
+	 public int loadStateCodeData(String csvFilePath) throws CensusAnalyserException {
+	        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
+	            CsvToBeanBuilder<CSVStateCode> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+	            csvToBeanBuilder.withType(CSVStateCode.class);
+	            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+	            CsvToBean<CSVStateCode> csvToBean = csvToBeanBuilder.build();
+	            Iterator<CSVStateCode> stateCodeCSVIterator = csvToBean.iterator();;
+	            Iterable<CSVStateCode> csvIterable = () -> stateCodeCSVIterator;
+	            int numOfEateries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
+	            return numOfEateries;
+	        } catch (IOException e) {
+	            throw new CensusAnalyserException(e.getMessage(),
+	                                              CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+	        } catch (IllegalStateException e) {
+	            throw new CensusAnalyserException(e.getMessage(),
+	                                             CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+	        } catch (RuntimeException e) {
+	            throw new CensusAnalyserException("Invalid header or Delimiter",
+	                                            CensusAnalyserException.ExceptionType.INVALID_HEADER_AND_DELIMITER);
+	        }
+	    }
 }
