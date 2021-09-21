f31f7cdf973c0ecc37c20cd612f3c1f82008a68f
namancg <naman.c@perfios.com>
2021-09-21 19:29:40 +0530

    ADDED: ability to load the information from csv file

---
+++ src/main/java/com/bridgelabz/statecensusanalyser/StateCensusAnalyser.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.statecensusanalyser;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.stream.StreamSupport;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class StateCensusAnalyser {
+	public int LoadCensusData(String csvFilePath) throws CensusAnalyserException {
+		try {
+	        Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+	        CsvToBeanBuilder<CSVStateCensus> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+	        csvToBeanBuilder.withType(CSVStateCensus.class);
+	        csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+	        CsvToBean<CSVStateCensus> csvToBean = csvToBeanBuilder.build();
+	        Iterator<CSVStateCensus> censusCSVIterator = csvToBean.iterator();;
+	        Iterable<CSVStateCensus> csvIterable = () -> censusCSVIterator;
+	        int numOfEateries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
+	        return numOfEateries;
+	    } catch (IOException e) {
+	        throw new CensusAnalyserException(e.getMessage(),
+	                                          CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+	    } catch (IllegalStateException e) {
+	        throw new CensusAnalyserException(e.getMessage(),
+	                                         CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+	    } catch (RuntimeException e) {
+	        throw new CensusAnalyserException("Invalid header or Delimiter",
+	                                         CensusAnalyserException.ExceptionType.INVALID_HEADER_AND_DELIMITER);
+	    }
+	}
+}
